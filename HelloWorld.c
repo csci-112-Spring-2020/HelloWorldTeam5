d0e9d92717d491a8a0bf87c89b3a37ab1e99db2f
A. Stauffer <63657884+BlackDogWarth@users.noreply.github.com>
2020-04-14 09:55:48 -0700

    Update HelloWorld.c

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,1 +1,14 @@
-#define 
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void hello_world(void);
+
+void main()
+{
+  helloworld();
+}
+
+void hello_world(void)
+{
+  printf("Hello World, from the tutor.");
+}
