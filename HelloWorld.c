de0582353f3a3e8f0924c38b729a1a7813407390
alexhernandez930 <44177106+alexhernandez930@users.noreply.github.com>
2020-04-14 10:02:18 -0700

    Update HelloWorld.c

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,10 +1,16 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
-int main(void)
-{
-  
-  printf("Hello World");
-  
-}
 
+int main()
+{
+
+	char name[100];
+	printf("Hello World!!\n");
+	printf("What is your name: ");
+	scanf("%s", &name);
+	printf("Hello %s", name);
+	
+
+
+}
